518f295bf142215ea37d8e02007cc3320b668a38
tangoyankee <miller.tim108@gmail.com>
2023-07-02 14:18:51 -0400

    health check

--- api/src/index.js
+++ api/src/index.js
@@ -4,8 +4,16 @@ const host = `127.0.0.1`;
 const port = 8000;
 
 const requestListener = (req, res) => {
-    res.writeHead(200);
-    res.end("My first server!");
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/":
+            res.writeHead(200);
+            res.end(JSON.stringify({ message: "Okay" }));
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({ message: "Resource not found" }));
+    }
 };
 
 const server = http.createServer(requestListener);
