d9afae5223d40927883c42f6a958fb17fe6eeaad
tangoyankee <miller.tim108@gmail.com>
2023-07-02 14:39:44 -0400

    return a single point feature

--- api/src/index.js
+++ api/src/index.js
@@ -3,12 +3,33 @@ const http = require('http');
 const host = `127.0.0.1`;
 const port = 8000;
 
+const point = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -76.51429143703932,
+          42.46254403506475
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+
 const requestListener = (req, res) => {
     res.setHeader("Content-Type", "application/json");
     switch (req.url) {
         case "/":
             res.writeHead(200);
             res.end(JSON.stringify({ message: "Okay" }));
+            break
+        case "/point":
+            res.writeHead(200);
+            res.end(JSON.stringify(point));
             break
         default:
             res.writeHead(404);
